ed476e50fe3d089d8133c5c4417c29914d8ea7f5
Phillip Liu <phillip@signalfx.com>
2016-08-02 14:05:56 -0700

    latest

--- src/com/signalfx/imgmap/ToHeatMap.java
+++ src/com/signalfx/imgmap/ToHeatMap.java
@@ -8,7 +8,6 @@ import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
-import java.util.Date;
 
 import javax.imageio.ImageIO;
 
@@ -26,12 +25,12 @@ import com.signalfx.metrics.protobuf.SignalFxProtocolBuffers.MetricType;
 
 
 public class ToHeatMap {
-    private final static String SFXURL = "http://lab-ingest.corp.signalfuse.com:8080";
-    private final static String SFXTOKEN = "";
+    private final static String SFXURL = "https://mon-ingest.signalfx.com";
+    private final static String SFXTOKEN = "PUT IN YOUR TOKEN";
+
     
     public static void main(String[] args) {
         File imageFile = getImageFile(args);
-        
         try {
             sendImageAsTS(getImage(imageFile), getSender(), imageFile.getName());
         } catch (IOException e) {
@@ -99,7 +98,7 @@ public class ToHeatMap {
             for (int row=0, cnt=0; row < image.getHeight(); row++) {
                 for (int col=0; col < image.getWidth(); col++, cnt++) {
                     Datum datum = SignalFxProtocolBuffers.Datum.newBuilder()
-                                   .setIntValue(image.getRGB(row, col))
+                                   .setIntValue(image.getRGB(col, row))
                                    .build();
                     
                     DataPoint dp = SignalFxProtocolBuffers.DataPoint.newBuilder()
@@ -118,11 +117,14 @@ public class ToHeatMap {
             session.close();
         }
     }
-    
+
     final static class Dimensions {
+        final static int [] sizeTable = { 9, 99, 999, 9999, 99999, 999999, 9999999,
+                                          99999999, 999999999, Integer.MAX_VALUE };
         final static DateFormat dateFormat = new SimpleDateFormat("MMddyyyy:HH:mm:ss");
         Dimension image = null;
         Dimension size = null;
+        private int maxDigits = 0;
         
         Dimensions(String image, int width, int height) {
             this.image = SignalFxProtocolBuffers.Dimension.newBuilder()
@@ -131,6 +133,7 @@ public class ToHeatMap {
             this.size = SignalFxProtocolBuffers.Dimension.newBuilder()
                           .setKey("size")
                           .setValue("" + width + "x" + height).build();
+            this.maxDigits = stringSize(width * height);
         }
         
         Dimension getPosition(int pos) {
@@ -138,18 +141,22 @@ public class ToHeatMap {
                     .setKey("position")
                     .setValue(getAlpha(pos)).build();
         }
+
+        static int stringSize(int x) {
+            for (int i=0; ; i++) {
+                if (x <= sizeTable[i]) {
+                    return i+1;
+                }
+            }
+        }
         
         private String getAlpha(int num) {
-            String result = "";
-            while (num > 0) {
-                num--; // 1 => a, not 0 => a
-                int remainder = num % 26;
-                char digit = (char) (remainder + 97);
-                result = digit + result;
-                num = (num - remainder) / 26;
+            int numDigits = stringSize(num);
+            String digits = Integer.toString(num);
+            for (int i=0; numDigits + i < maxDigits; i++) {
+                digits = "0" + digits;
             }
-
-            return result;
+            return digits;
         }
     }
     
